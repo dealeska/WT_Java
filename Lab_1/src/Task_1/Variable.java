61911271561df511b3c7267bee21a9a2c3df3b0b
dealeska <zenya.com44@gmail.com>
2021-09-24 15:54:54 +0300

    First 3 tasks done

---
+++ Lab_1/src/Task_1/Variable.java
@@ -0,0 +1,19 @@
+package Task_1;
+
+final public class Variable {
+    private float x;
+    private float y;
+
+    public Variable(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+}
